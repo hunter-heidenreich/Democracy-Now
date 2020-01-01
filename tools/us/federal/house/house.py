935f7209a6fa84c1f647e41f60583324cadb0791
hunter-heidenreich <hunter.scott.heidenreich@gmail.com>
2020-01-01 12:47:33 -0500

    Creating indices to search over bills with

--- tools/us/federal/house/house.py
+++ tools/us/federal/house/house.py
@@ -66,60 +66,61 @@ class USHouse:
             for p in pos:
                 self._search_by['rep']['state'][p].add(rep)
 
+        # Simple bill lookups
         self._search_by['bill']['sponsor url'] = defaultdict(set)
+        self._search_by['bill']['sponsor'] = defaultdict(set)
+        self._search_by['bill']['progress'] = defaultdict(set)
+        self._search_by['bill']['congress'] = defaultdict(set)
+        self._search_by['bill']['chamber'] = defaultdict(set)
+        self._search_by['bill']['subject - main'] = defaultdict(set)
+        self._search_by['bill']['subject - all'] = defaultdict(set)
+
         for bill in self._bills:
             self._search_by['bill']['sponsor url'][bill.get_overview()['sponsor']['url']].add(bill)
+            self._search_by['bill']['sponsor'][self._search_by['rep']['url'][bill.get_overview()['sponsor']['url']]].add(bill)
+            self._search_by['bill']['progress'][bill.get_progress()].add(bill)
+            self._search_by['bill']['congress'][bill.get_congress()].add(bill)
 
-    def count_bills_by(self, show=False):
-        bills_by = defaultdict(lambda: defaultdict(int))
-
-        # Bills by Party
-        for r, bs in self._search_by['bill']['sponsor url'].items():
-            party = self._search_by['rep']['url'][r].get_current_party()
-            bills_by['party'][party] += len(bs)
-
-        # Bills by State
-        for r, bs in self._search_by['bill']['sponsor url'].items():
-            states = self._search_by['rep']['url'][r].get_states()
-
-            for s in states:
-                bills_by['state'][s] += len(bs)
-
-        # Bills by Progress
-        for bill in self._bills:
-            bills_by['progress'][bill.get_progress()] += 1
-
-        # Bills by Subject
-        for bill in self._bills:
-            subjects = bill.get_subjects()
-            if 'main' in subjects:
-                bills_by['subject - main'][subjects['main']['title']] += 1
-                bills_by['subject - all'][subjects['main']['title']] += 1
-            for sub in subjects['others']:
-                bills_by['subject - all'][sub['title']] += 1
-
-        # Bills by Congress
-        for bill in self._bills:
-            bills_by['congress'][bill.get_congress()] += 1
-
-        # Bills by chamber
-        for bill in self._bills:
             title = bill.get_overview()['sponsor']['title']
             if title == 'Rep.':
                 title = 'House'
             elif title == 'Sen.':
                 title = 'Senate'
 
-            bills_by['chamber'][title] += 1
+            self._search_by['bill']['chamber'][title].add(bill)
+
+            subjects = bill.get_subjects()
+            if 'main' in subjects:
+                self._search_by['bill']['subject - main'][subjects['main']['title']].add(bill)
+                self._search_by['bill']['subject - all'][subjects['main']['title']].add(bill)
+            for sub in subjects['others']:
+                self._search_by['bill']['subject - all'][sub['title']].add(bill)
+
+        # Complex Bill lookups
+        self._search_by['bill']['party'] = defaultdict(set)
+        self._search_by['bill']['state'] = defaultdict(set)
+        for rep, bs in self._search_by['bill']['sponsor'].items():
+            party = rep.get_current_party()
+            self._search_by['bill']['party'][party] |= set(bs)
+
+            for s in rep.get_states():
+                self._search_by['bill']['state'][s] |= set(bs)
+
+    def count_bills_by(self, show=False):
+        bill = self._search_by['bill']
+        bill = {prop: {k: len(v) for k, v in se.items()} for prop, se in bill.items()}
 
         if show:
-            pprint(bills_by)
+            pprint(bill)
         else:
-            return bills_by
+            return bill
 
 
 if __name__ == '__main__':
     house = USHouse()
     house.read_files()
     house.generate_search_by()
-    house.count_bills_by(show=True)
+    cnts = house.count_bills_by()
+
+    import pdb
+    pdb.set_trace()
