2307a281a66d731cefef9ae493b9d345497df2d2
Hunter Heidenreich <hunter.scott.heidenreich@gmailcom>
2020-01-15 09:55:23 -0500

    Initialize basic query visualization

--- website/demviz/urls.py
+++ website/demviz/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.CoreIndex.as_view(), name='index'),
+    path('search/<str:cls>', views.QueryResult.as_view(), name='qres')
 ]
--- website/demviz/views.py
+++ website/demviz/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 
 class CoreIndex(TemplateView):
     template_name = 'demviz/index.html'
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+class QueryResult(ListView):
+    template_name = 'demviz/queryres.html'
+    context_object_name = 'results'
+
+    def get_queryset(self):
+        return []
