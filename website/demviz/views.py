74afeaea69e66d59f30de21cf106d94719073384
Hunter Heidenreich <hunter.scott.heidenreich@gmailcom>
2020-01-15 09:32:13 -0500

    Get basic layout of the core of the first page

--- website/demviz/views.py
+++ website/demviz/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+class CoreIndex(TemplateView):
+    template_name = 'demviz/index.html'
 
 
 def index(request):
--- website/website/settings.py
+++ website/website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'demviz.apps.DemvizConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
