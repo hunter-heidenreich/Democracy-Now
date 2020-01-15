63936b9aef3d9d67adabb5ddfd2eb8645048965a
Hunter Heidenreich <hunter.scott.heidenreich@gmailcom>
2020-01-15 10:12:37 -0500

    Add splits between the query types

--- website/demviz/views.py
+++ website/demviz/views.py
@@ -13,5 +13,22 @@ class QueryResult(ListView):
     template_name = 'demviz/queryres.html'
     context_object_name = 'results'
 
+    def __init__(self):
+        self.cls = None
+        ListView.__init__(self)
+
+    def get(self, request, *args, **kwargs):
+        if 'cls' in kwargs:
+            self.cls = kwargs['cls']
+
+        self.object_list = self.get_queryset()
+
+        context = self.get_context_data()
+        return self.render_to_response(context)
+
     def get_queryset(self):
+        if self.cls == 'reps':
+            return []
+        elif self.cls == 'bills':
+            return []
         return []
