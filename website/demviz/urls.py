ef4126c07c6da552003f5094a6f1dd042aaf25d9
Hunter Heidenreich <hunter.scott.heidenreich@gmailcom>
2020-01-19 10:44:41 -0500

    Adjusting css for extended search bar via address

--- website/demviz/urls.py
+++ website/demviz/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.CoreIndex.as_view(), name='index'),
-    path('search/<str:cls>', views.QueryResult.as_view(), name='qres')
+    path('search/<str:cls>', views.QueryResult.as_view(), name='qres'),
+    path('rep/<str:name>', views.CoreIndex.as_view(), name='repsview')
 ]
